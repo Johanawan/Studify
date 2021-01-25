e292b563b51bcd51bfcdffa685297fcf37023805
johanawan <johanawan@hotmail.com>
2021-01-25 11:55:26 +0000

    Completed calculator

--- calculator/urls.py
+++ calculator/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import calculator
 
 urlpatterns = [
-    path("calculator/", calculator, name="calculator"),
+    path("calculate/", calculator, name="calculator"),
 ]
