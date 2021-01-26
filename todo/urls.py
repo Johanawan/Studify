03cfcb9a1fa29c7868ca2de9cf4f890274c42fa2
johanawan <johanawan@hotmail.com>
2021-01-26 16:46:09 +0000

    Added functionality. Todo- CSS styling.

--- todo/urls.py
+++ todo/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import todo
+from .views import todo, updateTask, deleteTask
 
 urlpatterns = [
     path("", todo, name="todo"),
+    path("update-task/<str:pk>/", updateTask, name="update-task"),
+    path("delete-task/<str:pk>/", deleteTask, name="delete-task"),
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,5 +1,51 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import *
+from .forms import TaskForm
 
 # Create your views here.
 def todo(request):
-    return render(request, "todo/todo.html")
+    tasks = Task.objects.all()
+
+    form = TaskForm()
+
+    if request.method == "POST":
+        form = TaskForm(request.POST)
+        print(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect("/todo")
+    
+    context = {
+        "tasks": tasks,
+        "form": form,
+    }
+    return render(request, "todo/todo.html", context)
+
+def updateTask(request, pk):
+    task = Task.objects.get(id=pk)
+
+    form = TaskForm(instance=task)
+
+    if request.method == "POST":
+        form = TaskForm(request.POST, instance=task)
+        if form.is_valid():
+            form.save()
+        return redirect("/todo")
+
+    context = {
+        "form": form,
+    }
+    return render(request, "todo/update_task.html", context)
+
+def deleteTask(request, pk):
+    item = Task.objects.get(id=pk)
+
+    if request.method == "POST":
+        item.delete()
+        return redirect("/todo")
+
+    context = {
+        "item": item,
+    }
+
+    return render(request, "todo/delete.html", context)
