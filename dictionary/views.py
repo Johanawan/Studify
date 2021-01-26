8d8fbcdadd2de63e8decb674ff7ce598ef7e0712
johanawan <johanawan@hotmail.com>
2021-01-26 01:11:21 +0000

    finished building dictionary app.

--- dictionary/views.py
+++ dictionary/views.py
@@ -8,6 +8,10 @@ def dictionaryView(request):
     dictionaryWord = []
     word = "Look up something!"
 
+    word_type = None
+    definition = None
+    example = None
+
     if request.method == "POST":
         # Get the input data from the page
         language = request.POST.get("language")
@@ -17,47 +21,33 @@ def dictionaryView(request):
         url = "https://api.dictionaryapi.dev/api/v2/entries/{}/{}".format(language, lookup_word)
         
         response = requests.get(url).json()
-        
-        word_types = []
-        definitions = []
-        examples = []
-        synonyms = []
-        dictionaryWord = []
 
         try:
             word = response[0]["word"]
-            for i in range(len(response[0]["meanings"])):
-                word_type = response[0]["meanings"][i]["partOfSpeech"]
-                word_types.append(word_type)
 
-                try:
-                    definition = response[0]["meanings"][i]["definitions"][0]["definition"]
-                    definitions.append(definition)
-                except KeyError:
-                    definition = ""
-                    definitions.append(definition)
+            word_type = response[0]["meanings"][0]["partOfSpeech"].capitalize()
 
-                try:
-                    example = response[0]["meanings"][i]["definitions"][0]["example"]
-                    examples.append(example)
-                except KeyError:
-                    example = ""
-                    examples.append(example)
-                try:
-                    synonym = response[0]["meanings"][i]["definitions"][0]["synonyms"]
-                    synonyms.append(synonym)
-                except KeyError:
-                    synonym = ""
-                    synonyms.append(synonym)
+            try:
+                definition = response[0]["meanings"][0]["definitions"][0]["definition"]
+            except KeyError:
+                definition = ""
+
+            try:
+                example = response[0]["meanings"][0]["definitions"][0]["example"]
+            except KeyError:
+                example = ""
+
         except KeyError:
             print("We could not find the definition of the word you are looking for")
 
-        dictionaryWord = list(zip(word_types, definitions, examples, synonyms))
-    
+        # dictionaryWord = list(zip(titles, word_types, definitions, examples, synonyms))
+
     word_caps = word.capitalize()
     context = {
         "word": word_caps,
-        "dictionary_word": dictionaryWord,
+        "word_type": word_type,
+        "definition": definition,
+        "example": example,
     }
 
     return render(request, "dictionary/dictionary.html", context)
