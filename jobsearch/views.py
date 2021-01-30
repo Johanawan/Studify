c07cad7f482e543f5f8505ebd1f7fb1d7065327f
johanawan <johanawan@hotmail.com>
2021-01-30 12:52:09 +0000

    Built function to loop through pages and get all job data.

--- jobsearch/views.py
+++ jobsearch/views.py
@@ -16,49 +16,67 @@ def jobSearch(request):
         job_title = job_title_raw.replace(" ", "+")
         job_location = job_location_raw.replace(" ", "+")
 
-        # Create url format link
-        url = "https://uk.indeed.com/jobs?q={}&l={}".format(job_title, job_location)
+        # List of the first 4 pages of the paginator
+        pages = ["", "10", "20", "30"]
 
+        list_of_listings = []
 
-        html = requests.get(url)
-        #print(html.status_code)
+        for page in pages:
+            # Create url format link
+            url = "https://uk.indeed.com/jobs?q={}&l={}&start={}".format(job_title, job_location, page)
 
-        src = html.content
-        bs = BeautifulSoup(src, "html.parser")
-        
-        job_listings = bs.find_all("div", {"class": "jobsearch-SerpJobCard"})
+            html = requests.get(url)
+            #print(html.status_code)
 
-        print(len(job_listings))
-
-        listings = {}
-
-        for listing in job_listings:
+            src = html.content
+            bs = BeautifulSoup(src, "html.parser")
             
-            # Get job title
-            job_title = listing.find("a")["title"]
-            listings["job_title"] = job_title
-            print(listing)
-            
-            company = listing.find("span", {"class": "company"}).get_text(strip=True)
-            listings["company"] = company
+            job_listings = bs.find_all("div", {"class": "jobsearch-SerpJobCard"})
 
-            try:
-                location = listing.find("div", {"class": "location"}).get_text()
-                listings["location"] = location
-            except AttributeError:
-                listings["location"] = "-"
-            try:
-                salary = listing.find("span", {"class": "salaryText"}).text[2:]
-                listings["salary"] = salary
-            except AttributeError:
-                listing["salary"] = "-"
-            date_added = listing.find("span", {"class": "date"}).get_text()
-            listings["date_added"] = date_added
+            print(len(job_listings))
 
-            
-            break
+            listings = {}
 
-        print(listings)
+            for listing in job_listings:
+                
+                # Get job title
+                job_title = listing.find("a", {"title": True})["title"]
+                listings["job_title"] = job_title
+                
+                company = listing.find("span", {"class": "company"}).get_text(strip=True)
+                listings["company"] = company
+
+                # Get location
+                try:
+                    location = listing.find("span", {"class": "location"}).get_text()
+                    listings["location"] = location
+                except AttributeError:
+                    listings["location"] = job_location
+
+                # Get salary data
+                try:
+                    salary = listing.find("span", {"class": "salaryText"}).text[2:]
+                    listings["salary"] = salary
+                except AttributeError:
+                    listing["salary"] = "-"
+
+                # Get date added
+                date_added = listing.find("span", {"class": "date"}).get_text()
+                listings["date_added"] = date_added
+
+                dict1 = listings.copy()
+
+                #print(dict1)
+                
+                # Append listings to list of listings
+                list_of_listings.append(dict1)
+                # print(list_of_listings)
+                # print(listings)
+                listings.clear()
+                # print(listings)
+                
+        print(list_of_listings)
+            #print(listings)
 
     context = {
 
