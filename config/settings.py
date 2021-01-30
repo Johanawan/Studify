50946c7576c23be66dba798c79a7b4f9b8bb5e04
johanawan <johanawan@hotmail.com>
2021-01-30 22:39:10 +0000

    Outputted youtube data onto frontend.

--- config/settings.py
+++ config/settings.py
@@ -25,6 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env.str("SECRET_KEY")
+GOOGLE_API_KEY = env.str("GOOGLE_API_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DEBUG", default=False)
--- youtube/views.py
+++ youtube/views.py
@@ -1,8 +1,68 @@
 from django.shortcuts import render
+from googleapiclient.discovery import build
+from config.settings import GOOGLE_API_KEY
+import requests
+
+from isodate import parse_duration
 
 # Create your views here.
 def youtube(request):
+
+    search_url = "https://www.googleapis.com/youtube/v3/search"
+    video_url = "https://www.googleapis.com/youtube/v3/videos"
+
+    search_params = {
+        "part": "snippet",
+        "q": "learn python",
+        "key": GOOGLE_API_KEY,
+        "maxResults": 9,
+        "type": "video",
+    }
+
+    video_ids = []
+
+    r = requests.get(search_url, params=search_params)
+    results = r.json()["items"]
+
+    for result in results:
+        video_ids.append(result["id"]["videoId"])
+    
+    video_params = {
+        "key": GOOGLE_API_KEY,
+        "part": "snippet, contentDetails",
+        "id": ",".join(video_ids),
+        "maxResults": 9,
+    }
+
+    r = requests.get(video_url, params=video_params)
+    
+    results = r.json()["items"]
+
+    videos = []
+    for result in results:
+        title = result["snippet"]["title"]
+        vid_id = result["id"]
+        duration = int(parse_duration(result["contentDetails"]["duration"]).total_seconds() // 60)
+        thumbnail = result["snippet"]["thumbnails"]["high"]["url"]
+
+        video_data = {
+            "title": title,
+            "id": vid_id,
+            "url": "https://www.youtube.com/watch?v={}".format(vid_id),
+            "duration": duration,
+            "thumbnail": thumbnail,
+        }
+
+        videos.append(video_data)
+    
+    # if request.method == 'POST':
+    #     search_term = request.POST.get("youtube")
+    #     print(search_term)
+
     context = {
-        
+        "videos": videos
     }
     return render(request, "youtube/youtube.html", context)
+
+    
+# GOOGLE_API_KEY=AIzaSyA1i-lhuJe7SUEbdo8Jd2-uWTvSpS5X6TA
