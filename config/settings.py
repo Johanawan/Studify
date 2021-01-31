a6c967d7af6dc416376c3378eb1016acfb1fc44d
johanawan <johanawan@hotmail.com>
2021-01-31 11:44:18 +0000

    Edited config file to include hosts.

--- config/settings.py
+++ config/settings.py
@@ -30,7 +30,7 @@ GOOGLE_API_KEY = env.str("GOOGLE_API_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DEBUG", default=False)
 
-ALLOWED_HOSTS = ["https://studify-portal.herokuapp.com/", "127.0.0.1:8000"]
+ALLOWED_HOSTS = ["https://studify-portal1.herokuapp.com/", '127.0.0.1']
 
 
 # Application definition
